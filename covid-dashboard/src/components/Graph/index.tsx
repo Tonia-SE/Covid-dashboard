2f73fd1c1cfe409d82882869aa37264d2ab1e7be
Tonia-SE <64790742+Tonia-SE@users.noreply.github.com>
2020-12-13 21:04:30 +0300

    feat: first draft with react

---
+++ covid-dashboard/src/components/Graph/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Graph extends React.Component {
+    render() {
+    return (
+        <div className='graph'>
+            <p>
+            Grath coming soon...
+            </p>
+        </div>
+    );}
+};
