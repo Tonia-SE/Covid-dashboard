5528d97b7de099dfa609c017a2165209a83f5b91
Tonia-SE <64790742+Tonia-SE@users.noreply.github.com>
2020-12-15 11:32:32 +0300

    remove header

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -12,9 +12,10 @@ export class Map extends React.Component {
                     <Switcher />
                     <Maximise />
                 </div>
-                <div>
+                <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+                    <h1 className="h2">COVID-19 Dashboard</h1>
                     <MapWrapper />
-                </div>
+                </div>    
             </>
         );
     }
