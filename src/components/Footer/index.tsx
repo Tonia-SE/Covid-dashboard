cc879ea40b0b4b404dba192be57fe5c34e9bfab9
Tatsiana <pannage@gmail.com>
2020-12-22 01:22:23 +0300

    fix: fixed adaptive markup

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,9 +8,11 @@ export class Footer extends React.Component {
                 <div className="footer-logo"></div>
                 <div className="footer-copyright text-right">
                     <div className="copyright-title">© 2020 Copyright:</div>
-                    <div><a href="https://github.com/Tonia-SE" target="blank">https://github.com/Tonia-SE</a></div>
-                    <div><a href="https://github.com/pannage" target="blank">https://github.com/pannage</a></div>
-                    <div><a href="https://github.com/efando" target="blank">https://github.com/efando</a></div>
+                    <div className="copyright-wrapper">
+                        <div><a href="https://github.com/Tonia-SE" target="blank">https://github.com/Tonia-SE</a></div>
+                        <div><a href="https://github.com/pannage" target="blank">https://github.com/pannage</a></div>
+                        <div><a href="https://github.com/efando" target="blank">https://github.com/efando</a></div>
+                    </div>
                 </div>
             </div>
         );
