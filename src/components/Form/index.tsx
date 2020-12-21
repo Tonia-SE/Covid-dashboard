6a8b1b1bf94cb580430c98824a6df5f5675b9250
efandor <firstdir@gmail.com>
2020-12-21 11:37:38 +0300

    feat: add keyboard

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,23 +1,76 @@
 import React from 'react';
+import { render } from "react-dom";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 import './form.scss';
-
 interface Props {
     onfilterchange: (value: string) => void
 }
 export class Form extends React.Component<Props> {
     state = {
-        value: ''
+        layoutName: "default",
+        input: ""
+      };
+      
+  commonKeyboardOptions = {
+    onChange: (value: any) => this.onChange(value),
+    onKeyPress: (button: any) => this.onKeyPress(button),
+    theme: "simple-keyboard hg-theme-default hg-layout-default",
+    physicalKeyboardHighlight: true,
+    syncInstanceInputs: true,
+    mergeDisplay: true,
+    debug: true
+  };
+
+  keyboardOptions = {
+    ...this.commonKeyboardOptions,
+    display: {
+      "{tab}": "tab ⇥",
+      "{backspace}": "⌫",
+      "{enter}": "enter ↵",
+      "{capslock}": "caps lock ⇪",
+      "{shiftleft}": "shift ⇧",
+      "{shiftright}": "shift ⇧",
+      "{space}": "space",
+    }
+  };
+
+      onChange (input: any) {
+        this.setState({ input });
+        this.props.onfilterchange(input);
+        console.log("Input changed", input);
       };
 
-      onInputChange (event: any) {
-        this.setState({value: event.target.value});
-        this.props.onfilterchange(event.target.value);
+      onKeyPress = (button: any) => {
+        console.log("Button pressed", button);
+        if (button === "{shift}" || button === "{lock}") this.handleShift();
       };
-
+    
+      handleShift = () => {
+        const layoutName = this.state.layoutName;
+    
+        this.setState({
+          layoutName: layoutName === "default" ? "shift" : "default"
+        });
+      };
+    
       render () {
         return (
         <div className="input-group mb-3">
-          <input type="text" className="form-control" value={this.state.value} onChange={(event) => this.onInputChange(event)} />
+          <input
+              type="text"
+              className="form-control"
+              value={this.state.input}
+              placeholder={"Country"}
+              onChange={(event) => this.onChange(event)}
+          />
+          <div className={"keyboardContainer"}>
+            <Keyboard
+              baseClass={"simple-keyboard-main"}
+              layoutName={this.state.layoutName}
+              {...this.keyboardOptions}
+            />
+          </div>
         </div>
         );
       }
